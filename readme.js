a1e70979839da809b08acd04b65f145ce9a6f2a4
Faisal Ahmed Fahim <146206158+fahim-ahmed10@users.noreply.github.com>
2024-11-18 22:37:09 +0600

    first commit

---
+++ readme.js
@@ -0,0 +1,8 @@
+/*  
+* npm create vite@latest my-project -- --template react
+* npm install -D tailwindcss postcss autoprefixer
+* npx tailwindcss init -p
+* npm i -D daisyui@latest
+* npm install firebase
+
+*/
